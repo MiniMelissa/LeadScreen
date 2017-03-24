59c2b70793e7fdb80c549e36e5aab49b83f81233
melissa <xumeng919@gmail.com>
2017-03-24 18:34:22 -0400

    connect a disclaimer activity

--- LeadScrren/app/src/main/java/com/example/xumeng/leadscreen/MainActivity.java
+++ LeadScrren/app/src/main/java/com/example/xumeng/leadscreen/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.xumeng.leadscreen;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+  /*  public void openWHWScreen(View view){
+        Intent intent = new Intent(this, WHWScreen.class);
+        startActivity(intent);
+    }
+
+    public void openLevelScreen(View view){
+        Intent intent = new Intent(this, LevelScreen.class);
+        startActivity(intent);
+    }
+
+    public void openSymptomsScreen(View view){
+        Intent intent = new Intent(this, SymptomsScreen.class);
+        startActivity(intent);
+    }
+
+    public void openSurveyScreen(View view){
+        Intent intent = new Intent(this, SurveyScreen.class);
+        startActivity(intent);
+    }
+
+    public void openEvaluationScreen(View view){
+        Intent intent = new Intent(this, EvaluationScreen.class);
+        startActivity(intent);
+    }
+    public void openGuideLineScreen(View view){
+        Intent intent = new Intent(this, GuideLineScreen.class);
+        startActivity(intent);
+    }*/
+    public void openDisclaimerScreen(View view){
+        Intent intent = new Intent(this, Disclaimer.class);
+        startActivity(intent);
+    }
+ /*   public void openAppScreen(View view){
+        Intent intent = new Intent(this, AppScreen.class);
+        startActivity(intent);
+    }
+    public void openReferenceScreen(View view){
+        Intent intent = new Intent(this, ReferenceScreen.class);
+        startActivity(intent);
+    }*/
+
 }
